359140f5f5e56faed41780b295327464cd47fc98
James Meyer <jamesjmeyer210@gmail.com>
2021-09-09 18:20:43 -0500

    added documentation comments

--- matrix-gateway/src/client/v1/matrix_client.rs
+++ matrix-gateway/src/client/v1/matrix_client.rs
@@ -25,7 +25,7 @@ pub struct MatrixClient
     api_uri: ApiUriBuilder,
     http_client: Client,
 }
-
+/// A template for building `GET` requests and mapping their `Err` to `MatrixClientErr`
 macro_rules! http_get {
     ($http_client:expr, $uri:expr) => {
         $http_client
@@ -35,7 +35,8 @@ macro_rules! http_get {
         .map_err(|e| MatrixClientError::SendRequestError(e))
     }
 }
-
+/// A template for building `POST` requests and mapping their `Err` to `MatrixClientErr`.
+/// This macro expects a body that serializes to json.
 macro_rules! http_post {
     ($http_client:expr, $uri:expr, $json:expr) => {
         $http_client
@@ -45,7 +46,8 @@ macro_rules! http_post {
         .map_err(|e| MatrixClientError::SendRequestError(e))
     }
 }
-
+/// A template for extracting json from `HttpResponse` and mapping both the `PayloadErr` and
+/// `JsonDeserializationError` to `MatrixClientErr`
 macro_rules! get_json {
     ($t:ty, $response:expr) => {
         {
@@ -59,7 +61,7 @@ macro_rules! get_json {
         }
     }
 }
-
+/// I don't even know how to explain this one. It basically does everything.
 macro_rules! try_convert_200 {
     ($http_response:expr, $model:ty) => {
         match $http_response.status()
@@ -81,18 +83,27 @@ impl MatrixClient
         }
     }
 
+    /// `GET` the authentication scheme of the matrix-synapse API
     pub async fn get_login(&self) -> Result<FlowCollection, MatrixClientError>
     {
         let mut response = http_get!(self.http_client, self.api_uri.login())?;
         try_convert_200!(response, FlowCollection)
     }
 
+    /// `POST` the credentials of a user and expect a `200` response with an access token
     pub async fn post_login(&self, req: &LoginRequest) -> Result<LoginResponse, MatrixClientError>
     {
         let mut response = http_post!(self.http_client, self.api_uri.login(), req)?;
         try_convert_200!(response, LoginResponse)
     }
 
+    /// `POST` a basic message and expect and expect a response that contains an event id
+    /// ```bash
+    /// curl -XPOST -d '{"msgtype":"m.text", "body":"hello"}' \
+    ///     "https://API/send/m.room.message?access_token=YOUR_ACCESS_TOKEN"
+    ///
+    /// { "event_id": "EVENT ID" }
+    /// ```
     pub async fn post_message(&self
         , msg: &MessageRequest
         , room_id: Encoded<&str>
@@ -108,52 +119,30 @@ impl MatrixClient
 #[cfg(test)]
 mod test
 {
-    use std::fs;
     use super::*;
-    use crate::model::UserCredential;
-    use crate::client::v1::model::{AuthenticationType, LoginIdentifier, IdentifierType};
+    use std::convert::TryFrom;
+    use std::path::Path;
+    use actix_web::client::Client;
 
-    /*
-    #[actix_rt::test]
-    async fn foo() -> ()
+    fn init_matrix_client() -> MatrixClient
     {
-        let config = fs::read_to_string(".client.json").unwrap();
-        let config = ClientConfig::try_from(config.as_str()).unwrap();
+        let config = ClientConfig::try_from(Path::new(".client.json")).unwrap();
+        let api_uri_builder = ApiUriBuilder::new(config.authority.as_str(), config.client_api.as_str()).unwrap();
+        let actix_http_client = Client::default();
 
-        let client = Client::default();
-
-        let matrix = MatrixClient::new(config, client).unwrap();
-        let x = matrix.get_login().await;
-
-        println!("{:?}", x);
+        MatrixClient::new(api_uri_builder, actix_http_client)
     }
 
     #[actix_rt::test]
-    async fn foo2() -> ()
+    async fn matrix_client_returns_flow_on_successful_get_login()
     {
-        let config = fs::read_to_string(".client.json").unwrap();
-        let config = ClientConfig::try_from(config.as_str()).unwrap();
+        let matrix = init_matrix_client();
 
-        let client = Client::default();
+        let flow = matrix.get_login().await;
 
-        let matrix = MatrixClient::new(config, client).unwrap();
+        assert!(flow.is_ok());
 
-        let users = fs::read_to_string(".users.json").unwrap();
-        let users: Vec<UserCredential> = serde_json::from_str(users.as_str()).unwrap();
-
-        let user = users.get(0).unwrap();
-
-        let req = LoginRequest {
-            auth_type: AuthenticationType::Password,
-            identifier: LoginIdentifier {
-                id_type: IdentifierType::User,
-                user: user.user_name.clone(),
-            },
-            password: user.password.clone(),
-        };
-
-        let res = matrix.post_login(&req).await;
-
-        println!("{:?}", res);
-    }*/
+        let flow = flow.ok().unwrap().flows;
+        assert!(flow.len() >= 1);
+    }
 }
--- matrix-gateway/src/client/v1/mod.rs
+++ matrix-gateway/src/client/v1/mod.rs
@@ -9,34 +9,3 @@ use model::{Flow, FlowCollection, AuthenticationType};
 pub use api_uri_builder::ApiUriBuilder as ApiUriBuilder;
 pub use client_config::ClientConfig as ClientConfig;
 pub use matrix_client::MatrixClient as MatrixClient;
-
-#[cfg(test)]
-mod test
-{
-    use super::*;
-    use std::convert::TryFrom;
-    use std::path::Path;
-    use actix_web::client::Client;
-
-    fn init_matrix_client() -> MatrixClient
-    {
-        let config = ClientConfig::try_from(Path::new(".client.json")).unwrap();
-        let api_uri_builder = ApiUriBuilder::new(config.authority.as_str(), config.client_api.as_str()).unwrap();
-        let actix_http_client = Client::default();
-
-        MatrixClient::new(api_uri_builder, actix_http_client)
-    }
-
-    #[actix_rt::test]
-    async fn matrix_client_returns_flow_on_successful_get_login()
-    {
-        let matrix = init_matrix_client();
-
-        let flow = matrix.get_login().await;
-
-        assert!(flow.is_ok());
-
-        let flow = flow.ok().unwrap().flows;
-        assert!(flow.len() >= 1);
-    }
-}
