4df2bb329c43dd81cc26fe5cfee2284a4529c93f
James Meyer <jamesjmeyer210@gmail.com>
2021-09-16 15:30:24 -0500

    moved tests to external module

--- matrix-http-client/src/lib.rs
+++ matrix-http-client/src/lib.rs
@@ -1,11 +1,12 @@
 mod api_uri_builder;
 mod client_config;
-mod constants;
 mod matrix_client;
-mod model;
 mod text_message;
-mod abstraction;
-mod error;
+
+pub mod abstraction;
+pub mod model;
+pub mod error;
+pub mod constants;
 
 pub use abstraction::TMatrixClient;
 pub use api_uri_builder::ApiUriBuilder;
